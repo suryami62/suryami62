0e2fad8b1491f9b29f5cd7df180c0267592d0df7
suryami62 <suryaarya098@gmail.com>
2024-12-31 09:26:35 +0700

    Feat add Prerender

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 using suryami62;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+if (!builder.RootComponents.Any())
+{
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+}
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
+
 builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+    services.AddHeadElementHelper();
+    services.AddBlazoredLocalStorage();
+}
