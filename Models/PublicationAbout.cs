44735ad4d24fe8970f9f1dc8f7aa025c7a9989ea
suryami62 <suryami62@outlook.com>
2025-11-28 07:42:21 +0700

    init: V3 replace Blazor WASM with Blazor Web App

--- Models/PublicationAbout.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace suryami62.Models;
-
-public record PublicationAbout(PublicationData? PublicationData);
-
-public record PublicationData(StaticPage? StaticPage);
-
-public record StaticPage(string? Title, Content? Content);
-
-public record Content(string? Markdown);
